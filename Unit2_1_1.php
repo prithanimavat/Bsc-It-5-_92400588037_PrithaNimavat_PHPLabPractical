d7a5b814784a1f50e460026f50c9b7599cd2f345
prithanimavat <prithanimavat@gmail.com>
2026-06-12 09:06:56 +0530

    Add files via upload

---
+++ Unit2_1_1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+$daysofWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+echo "Numeric Array (Days) \n";
+print_r($daysofWeek)
+
+
+?>
+
+        
+</body>
+</html>
